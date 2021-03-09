ba6470c2765f593953a8e562a38c6814ccf4aec0
seth <seth.j.near@gmail.com>
2021-03-09 15:15:11 -0600

    commit tree feff688f0028c832a6aac2bd74bf7a9f689bfdf5

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,47 @@
 export default (state = [], action) => {
-  return state;
+  let i;
+  let quote;
+
+  switch (action.type) {
+    case 'ADD_QUOTE':
+      return state.concat(action.quote)
+
+      case 'REMOVE_QUOTE':
+        return state.filter(q => q.id !== action.quoteId)
+
+        case 'UPVOTE_QUOTE':
+          i = state.findIndex(q => q.id === action.quoteId)
+          quote = state[i]
+          return [...state.slice(0, i), Object.assign({}, quote, {votes: quote.votes += 1}), ...state.slice(i + 1)]
+          // index = state.findIndex(quote => quote.id === action.quoteId);
+          // quote = state[index];
+    
+          // return [
+          //   ...state.slice(0, index),
+          //   Object.assign({}, quote, { votes: quote.votes += 1 }),
+          //   ...state.slice(index + 1)
+          // ];
+    
+          case 'DOWNVOTE_QUOTE':
+            i = state.findIndex(q => q.id === action.quoteId)
+            quote = state[i]
+            if (quote.votes > 0) {
+              return [...state.slice(0, i), Object.assign({}, quote, {votes: quote.votes -= 1}), ...state.slice(i + 1)]
+            }
+              return state
+            // index = state.findIndex(quote => quote.id === action.quoteId);
+            // quote = state[index];
+            // if (quote.votes > 0) {
+            //   return [
+            //     ...state.slice(0, index),
+            //     Object.assign({}, quote, { votes: quote.votes -= 1 }),
+            //     ...state.slice(index + 1)
+            //   ];
+            // }
+            // return state;
+    default:
+      return state;
+      
+  }
+  
 }
